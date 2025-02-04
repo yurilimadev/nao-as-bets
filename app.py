90acf1f2a9e011bf0be21f0fd190f8c3f785bc91
Yuri Dimitri Ribeiro Lima <dimitri.limaf@gmail.com>
2025-02-04 20:51:22 -0300

    corrigindo sintaxe de conexão para render

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{
-    DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('TOKEN_SEGURANCA')
 
